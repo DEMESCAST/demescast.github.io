3afc1770696f6a8ce1adedd9f17c55e90d430172
DEMESCAST <demescast@users.noreply.github.com>
2026-07-05 01:42:43 -0300

    Remove EP.27 Drielle Berto

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const episodios = [
     { num: "EP.55", titulo: "MARCELA RANGEL", desc: "Bate-papo com Marcela Rangel, diretora do Instituto Dom Barreto.", link: "https://www.youtube.com/watch?v=SpaA-YEM3z8&t=4244s", thumb: "https://img.youtube.com/vi/SpaA-YEM3z8/hqdefault.jpg" },
     { num: "EP.49", titulo: "ALLINE VASCONCELOS", desc: "Bate-papo com a influenciadora e jornalista Alline Vasconcelos sobre os assuntos mais comentados do momento.", link: "https://www.youtube.com/watch?v=bOxzCcRePlU", thumb: "https://img.youtube.com/vi/bOxzCcRePlU/hqdefault.jpg" },
     { num: "EP.44", titulo: "DÉBORA FACCHINETTI", desc: "A escritora Débora Facchinetti retorna ao podcast para falar sobre o lançamento do seu novo livro.", link: "https://www.youtube.com/watch?v=FbkRgjKvisM", thumb: "https://img.youtube.com/vi/FbkRgjKvisM/hqdefault.jpg" },
-    { num: "EP.27", titulo: "DRIELLE BERTO E MARCOS VINÍCIUS", desc: "Uma conversa esclarecedora sobre a Doença de Crohn com dois convidados especiais.", link: "https://www.youtube.com/watch?v=zStjlP39tfI", thumb: "https://img.youtube.com/vi/zStjlP39tfI/hqdefault.jpg" },
 ];
 
 const grid = document.getElementById('episodios-grid');
